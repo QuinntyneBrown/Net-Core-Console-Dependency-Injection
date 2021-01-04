e22eede293d1caa653d6a5a09969b722ff30f081
QuinntyneBrown <quinntynebrown@gmail.com>
2021-01-03 19:59:06 -0500

    initial

---
+++ src/Net-Core-Console-Dependency-Injection/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Net_Core_Console_Dependency_Injection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = BuildContainer(args);
+
+            var instance = host.Services.GetRequiredService<Foo>();
+
+            Console.WriteLine(instance.Env);
+
+            Console.ReadLine();
+        }
+
+        static IHost BuildContainer(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices(x =>
+                {
+                    x.AddTransient<Foo>();
+                })
+                .Build();
+        }
+    }
+
+    public class Foo
+    {
+        private readonly IHostEnvironment _env;
+        public Foo(IHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        public string Env => _env.EnvironmentName;
+    }
+}
+
+
