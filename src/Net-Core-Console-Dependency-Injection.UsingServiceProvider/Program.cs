963bae3b94c71d720abc5e78d99c97d484876fcb
QuinntyneBrown <quinntynebrown@gmail.com>
2021-01-03 20:15:20 -0500

    add both options

---
+++ src/Net-Core-Console-Dependency-Injection.UsingServiceProvider/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
+using System;
+
+namespace Net_Core_Console_Dependency_Injection.UsingServiceProvider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = BuildContainer(args);
+
+            var instance = container.GetRequiredService<Foo>();
+
+            Console.WriteLine(instance.Env);
+
+            Console.ReadLine();
+        }
+
+        static ServiceProvider BuildContainer(string[] args)
+        {
+            var services = new ServiceCollection();
+
+            services.AddTransient<Foo>();
+
+            services.AddSingleton<IHostEnvironment, HostingEnvironment>();
+
+            return services.BuildServiceProvider();
+
+        }
+    }
+
+    public class Foo
+    {
+        private readonly IHostEnvironment _env;
+        public Foo(IHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        public string Env => _env.EnvironmentName;
+    }
+}
+
